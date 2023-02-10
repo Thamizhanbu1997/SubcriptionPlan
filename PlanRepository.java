58a56484f6f3aa2bbab449f85c54fbdf721bc366
Thamizhanbu <122254524+Thamizhanbu1997@users.noreply.github.com>
2023-02-10 13:26:26 +0530

    Add files via upload

---
+++ PlanRepository.java
@@ -0,0 +1,15 @@
+package com.example.subcription;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+
+	
+
+	
+
+}
+
