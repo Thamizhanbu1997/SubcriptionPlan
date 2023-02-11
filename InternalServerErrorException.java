08d0e3a07b1b7b826d5a3ab0300d0b95ef28a733
Thamizhanbu <122254524+Thamizhanbu1997@users.noreply.github.com>
2023-02-11 12:15:31 +0530

    Add files via upload

---
+++ InternalServerErrorException.java
@@ -0,0 +1,16 @@
+package com.example.subcription;
+
+
+import org.springframework.dao.DataAccessException; 
+import org.springframework.http.HttpStatus; 
+import org.springframework.web.bind.annotation.ResponseStatus; 
+ 
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR) 
+public class InternalServerErrorException extends DataAccessException {
+
+	private static final long serialVersionUID = 1L; 
+	  
+    public InternalServerErrorException(String message) { 
+        super(message); 
+    }
+}
