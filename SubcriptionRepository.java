f2883f5a4a2c1a0a23de28bf834ebb132aa036e9
Thamizhanbu <122254524+Thamizhanbu1997@users.noreply.github.com>
2023-02-15 17:12:38 +0530

    Add files via upload

---
+++ SubcriptionRepository.java
@@ -0,0 +1,8 @@
+package com.example.subcription;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubcriptionRepository extends JpaRepository<Subcription, Long> {
+
+}
+ 
