143bbc183dc65d95a085e873fe77dd61f8780904
Thamizhanbu <122254524+Thamizhanbu1997@users.noreply.github.com>
2023-02-16 12:21:47 +0530

    Add files via upload

---
+++ PlanController.java
@@ -0,0 +1,50 @@
+package com.example.subcription;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/plan")
+public class PlanController {
+	@Autowired
+	PlanService planService;
+
+	@PostMapping
+	public Plan createPlan(@RequestBody Plan plan) {
+		return planService.save(plan);
+		
+	}
+    
+	@GetMapping(value = "{id}")
+	public Plan getPlan(@PathVariable("id") Long id) {
+		return planService.getById(plan) ;
+	}
+	@GetMapping
+	public List<Plan> getAllPlans() {
+		return planService.findAll();
+	}
+   
+	@PutMapping(value = "{id}")
+	public Plan updatePlan(@PathVariable ("id")Long id, Plan plan) {
+		return planService.updatePlan(id,plan);
+	}
+
+	@DeleteMapping(value = "{id}")
+	public Plan deletePlan(@PathVariable("id") Long id) {
+		return planService.getById(id);
+
+	}
+
+}
