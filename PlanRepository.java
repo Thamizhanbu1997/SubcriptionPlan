ddbd0c2e93ab2a0d5acb28bce87df61451fe6a65
Thamizhanbu <122254524+Thamizhanbu1997@users.noreply.github.com>
2023-02-21 11:31:20 +0530

    Add files via upload

--- PlanRepository.java
+++ PlanRepository.java
@@ -5,19 +5,17 @@ import java.util.List;
 
 import org.h2.mvstore.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 
-public interface PlanRepository extends JpaRepository<Plan, Long> {
-     String FILTER_PLANS_FIRST_NAME_AND_LAST_NAME_QUERY = null;
-
-	@Query("FILTER_PLANS_FIRST_NAME_AND_LAST_NAME_QUERY")
-     Page<Plan> findAll(String firstNameFilter, String lastNameFilter, Pageable pageable );
-
-	
+public interface PlanRepository extends JpaRepository<Plan, Long>, JpaSpecificationExecutor<Plan> {
      
+     public Page<Plan> findAll(Specification<Plan> spec, Pageable pageable);
+     
+     public List<Plan> findAll(Specification<Plan> spec); 
 	
 	}
 	
