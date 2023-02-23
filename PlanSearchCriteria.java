619880bd08932f471adda2ce50c18792c1345d6a
Thamizhanbu <122254524+Thamizhanbu1997@users.noreply.github.com>
2023-02-23 14:43:38 +0530

    Add files via upload

---
+++ PlanSearchCriteria.java
@@ -0,0 +1,26 @@
+package com.example.subcription;
+
+public class PlanSearchCriteria {
+
+	private String name;
+	private String page;
+	private String pageSize;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPage() {
+		return page;
+	}
+	public void setPage(String page) {
+		this.page = page;
+	}
+	public String getPageSize() {
+		return pageSize;
+	}
+	public void setPageSize(String pageSize) {
+		this.pageSize = pageSize;
+	}
+}
