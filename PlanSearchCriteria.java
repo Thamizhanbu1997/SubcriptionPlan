d9761585e7b959e929993894693df8b753389e68
Thamizhanbu <122254524+Thamizhanbu1997@users.noreply.github.com>
2023-02-23 15:12:46 +0530

    Add files via upload

--- PlanSearchCriteria.java
+++ PlanSearchCriteria.java
@@ -3,24 +3,24 @@ package com.example.subcription;
 public class PlanSearchCriteria {
 
 	private String name;
-	private String page;
-	private String pageSize;
+	private Integer page;
+	private Integer pageSize;
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPage() {
+	public Integer getPage() {
 		return page;
 	}
-	public void setPage(String page) {
+	public void setPage(Integer page) {
 		this.page = page;
 	}
-	public String getPageSize() {
+	public Integer getPageSize() {
 		return pageSize;
 	}
-	public void setPageSize(String pageSize) {
+	public void setPageSize(Integer pageSize) {
 		this.pageSize = pageSize;
+	}
+	public void setName(String name) {
+		this.name = name;
 	}
 }
