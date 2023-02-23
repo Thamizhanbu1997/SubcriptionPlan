921239283c89f4fdf83eb190437920ea2fc08677
Thamizhanbu <122254524+Thamizhanbu1997@users.noreply.github.com>
2023-02-23 17:57:06 +0530

    Add files via upload

--- PlanSpecification.java
+++ PlanSpecification.java
@@ -2,44 +2,39 @@ package com.example.subcription;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.function.Predicate;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
+import org.h2.table.Plan;
 import org.hibernate.Criteria;
 import org.springframework.data.jpa.domain.Specification;
 
-public class PlanSpecification implements Specification<Plan> {
-
-	 private List<SearchCriteria> list;
-
-	    public PlanSpecification() {
-	        this.list = new ArrayList<>();
-	    }
-
-	    public void add(SearchCriteria criteria) {
-	        list.add(criteria);
-	    }
-
-	    public Predicate toPredicate(Root<Plan> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-
-	    	 List<Predicate> predicates = new ArrayList<>();
-	    	 for(SearchCriteria criteria : list) {
-	    		  if (criteria.getOperation().equals(SearchOperation.STARTS_WITH)) {
-	                 predicates.add(builder.like(
-	                         builder.lower(root.get(criteria.getKey())),
-	                         "%" + criteria.getValue().toString().toLowerCase() + "%"));
-	             } else if (criteria.getOperation().equals(SearchOperation.ENDS_WITH)) {
-	                 predicates.add(builder.like(
-	                         builder.lower(root.get(criteria.getKey())),
-	                         criteria.getValue().toString().toLowerCase() + "%"));
-	             }
-	    	 }
-	    	 return builder.and(predicates.toArray(new Predicate[0]));
-	    }
-	    	 }
-	    
-	    	 
+public class PlanSpecification  {
+   
+	   private Specification<Plan> specification(PlanSearchCriteria criteria) {
+		return new Specification<Plan>() {
+		@Override
+		
+		public Predicate toPredicate(Root<Plan> root, CriteriaQuery<?> query, CriteriaBuilder cBuilder ) {
+			List<Predicate> predicates= new ArrayList<Predicate>();
+			if(criteria.getName() != null) {
+				predicates.add(cBuilder.like(cBuilder.lower(cBuilder.function("replace", String.class, root.get("name"), cBuilder.literal(""), cBuilder.literal(""))),
+						criteria.getName().toLowerCase().replace("", "") + "%"));
+			}
+			if(criteria.getPage()!=null) {
+				predicates.add(cBuilder.and(cBuilder.equal(root.get("page"),criteria.getPage())));
+			}
+			if(criteria.getPageSize()!=null) {
+				predicates.add(cBuilder.and(cBuilder.equal(root.get("pageSize"), criteria.getPageSize())));
+				
+			}
+			return cBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
+		}
+		
+		
+		};
+	}
+}
