96b99e08e211516a77b633594bc0155d6017914e
Thamizhanbu <122254524+Thamizhanbu1997@users.noreply.github.com>
2023-02-24 10:49:17 +0530

    Add files via upload

--- PlanSpecification.java
+++ PlanSpecification.java
@@ -2,27 +2,24 @@ package com.example.subcription;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
-import org.h2.table.Plan;
-import org.hibernate.Criteria;
 import org.springframework.data.jpa.domain.Specification;
 
 public class PlanSpecification  {
-   
+      
 	   private Specification<Plan> specification(PlanSearchCriteria criteria) {
-		return new Specification<Plan>() {
-		@Override
-		
-		public Predicate toPredicate(Root<Plan> root, CriteriaQuery<?> query, CriteriaBuilder cBuilder ) {
+		   return new Specification<Plan>() {
+			   @Override
+			   public Predicate toPredicate(Root<Plan> root, CriteriaQuery<?> query, CriteriaBuilder cBuilder) {
+			   
 			List<Predicate> predicates= new ArrayList<Predicate>();
 			if(criteria.getName() != null) {
-				predicates.add(cBuilder.like(cBuilder.lower(cBuilder.function("replace", String.class, root.get("name"), cBuilder.literal(""), cBuilder.literal(""))),
-						criteria.getName().toLowerCase().replace("", "") + "%"));
+				predicates.add(cBuilder.like(cBuilder.lower(root.get("name")),"%" + criteria.getName().toLowerCase() + "%"));
 			}
 			if(criteria.getPage()!=null) {
 				predicates.add(cBuilder.and(cBuilder.equal(root.get("page"),criteria.getPage())));
@@ -32,9 +29,8 @@ public class PlanSpecification  {
 				
 			}
 			return cBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
-		}
-		
-		
-		};
-	}
+			   }
+			   };
+	
 }
+	   }
